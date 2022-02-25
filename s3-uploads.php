d755574f3c0e8514349ffcb791e7b86a2a3ae0fb
Hyveli <94733949+Hyveli@users.noreply.github.com>
2022-02-25 18:00:13 -0500

    Fix Callable WP Command doesn't exist and Plugin Activation Error
    Fixes the following errors as of 2/25/2022;
    Error: Callable "S3_Uploads\\WP_CLI_Command" does not exist, and cannot be registered as `wp s3-uploads`.
    Error: 's3-uploads' is not a registered wp command. See 'wp help' for available commands.

--- s3-uploads.php
+++ s3-uploads.php
@@ -13,5 +13,6 @@ require_once __DIR__ . '/inc/namespace.php';
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
+    require_once dirname( __FILE__ ) . '/inc/class-wp-cli-command.php';
+    WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
 }
